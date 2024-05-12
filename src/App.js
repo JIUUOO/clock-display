08295194f2931c5cbed47d9b824d952c85d718ad
JIUUOO <nninjiuuoo@gmail.com>
2024-05-12 15:29:06 +0900

    feat: add get time only once

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <DateOfToday />
+    </div>
+  );
+}
+
+function DateOfToday() {
+  function makeFormat(num) {
+    return num <= 9 ? " " + num.toString() : num.toString();
+  }
+
+  let today = new Date();
+  let year = today.getFullYear().toString();
+  let month = makeFormat(today.getMonth() + 1);
+  let date = makeFormat(today.getDate());
+
+  const dayKr = (d) => {
+    switch (d) {
+      case 0:
+        return "일";
+      case 1:
+        return "월";
+      case 2:
+        return "화";
+      case 3:
+        return "수";
+      case 4:
+        return "목";
+      case 5:
+        return "금";
+      case 6:
+        return "토";
+    }
+  };
+  let day = dayKr(today.getDay());
+
+  let hour = makeFormat(today.getHours());
+  let minute = makeFormat(today.getMinutes());
+  let second = makeFormat(today.getSeconds());
+
+  return (
+    <div>
+      <h1>
+        {year +
+          "년 " +
+          month +
+          "월 " +
+          date +
+          "일 (" +
+          day +
+          ") " +
+          hour +
+          ":" +
+          minute +
+          ":" +
+          second}
+      </h1>
     </div>
   );
 }
